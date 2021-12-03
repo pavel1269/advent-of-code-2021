012c1a38a1b897250e86300257716f735b84d5e6
pavel1269 <pavelgrim@gmail.com>
2021-12-03 09:49:30 +0100

    Day 3, part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -5,7 +5,73 @@ pub fn get_solution_part1() -> i64 {
     return result;
 }
 
+pub fn get_solution_part2() -> i64 {
+    let diagnostic = get_input();
+    let result = calculate_life_support_rating(diagnostic);
+    return result;
+}
+
+fn calculate_life_support_rating(diagnostic: Vec<&'static str>) -> i64 {
+
+    let mut filtered_oxygen = diagnostic.clone();
+    let mut filtered_co2 = diagnostic.clone();
+    for index in 0..diagnostic.len() {
+        if filtered_oxygen.len() > 1 {
+            let common_bits = process_diagnostic(&filtered_oxygen);
+            let (zeroes, ones) = common_bits[index];
+            let oxygen_filter_value = if ones >= zeroes {
+                '1'
+            } else {
+                '0'
+            };
+
+            filtered_oxygen = filtered_oxygen.iter().filter(|number| number.as_bytes()[index] as char == oxygen_filter_value).cloned().collect();
+            // println!("oxygen[{}]: {:?}, filter: {}", index, filtered_oxygen, oxygen_filter_value);
+        }
+        
+        if filtered_co2.len() > 1 {
+            let common_bits = process_diagnostic(&filtered_co2);
+            let (zeroes, ones) = common_bits[index];
+            let co2_filter_value = if zeroes <= ones {
+                '0'
+            } else {
+                '1'
+            };
+
+            filtered_co2 = filtered_co2.iter().filter(|number| number.as_bytes()[index] as char == co2_filter_value).cloned().collect();
+            // println!("co2[{}]: {:?}, filter: {}", index, filtered_co2, co2_filter_value);
+        }
+    }
+
+    assert_eq!(filtered_co2.len(), 1);
+    assert_eq!(filtered_oxygen.len(), 1);
+
+    let oxygen_rating = i64::from_str_radix(filtered_oxygen[0], 2).unwrap();
+    let co2_rating = i64::from_str_radix(filtered_co2[0], 2).unwrap();
+
+    // println!("filtered_oxygen: {}, filtered_co2: {}", filtered_oxygen[0], filtered_co2[0]);
+
+    return oxygen_rating * co2_rating;
+}
+
 fn calculate_power_consumption(diagnostic: Vec<&'static str>) -> i64 {
+    let common_bits = process_diagnostic(&diagnostic);
+    let mut epsilon = 0;
+    let mut gamma = 0;
+    for (zeroes, ones) in common_bits {
+        epsilon <<= 1;
+        gamma <<= 1;
+        if ones > zeroes {
+            epsilon += 1;
+        } else {
+            gamma += 1;
+        }
+    }
+
+    return gamma * epsilon;
+}
+
+fn process_diagnostic(diagnostic: &Vec<&'static str>) -> Vec<(i32, i32)> {
     let mut common_bits = vec![(0, 0); diagnostic[0].len()];
 
     for diag_line in diagnostic {
@@ -24,19 +90,7 @@ fn calculate_power_consumption(diagnostic: Vec<&'static str>) -> i64 {
         }
     }
 
-    let mut epsilon = 0;
-    let mut gamma = 0;
-    for (zeroes, ones) in common_bits {
-        epsilon <<= 1;
-        gamma <<= 1;
-        if ones > zeroes {
-            epsilon += 1;
-        } else {
-            gamma += 1;
-        }
-    }
-
-    return gamma * epsilon;
+    return common_bits;
 }
 
 fn get_input() -> Vec<&'static str> {
@@ -77,5 +131,19 @@ mod tests {
         let result = get_solution_part1();
 
         assert_eq!(3320834, result);
+    }
+
+    #[test]
+    fn example_part2_correct_result() {
+        let result = calculate_life_support_rating(get_example_input());
+
+        assert_eq!(230, result);
+    }
+
+    #[test]
+    fn input_part2_correct_result() {
+        let result = get_solution_part2();
+
+        assert_eq!(4481199, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn get_known_solutions() -> Vec<[fn() -> i64; MAX_PARTS]> {
     let known_solutions = vec![
         [day01::get_solution_part1, day01::get_solution_part2],
         [day02::get_solution_part1, day02::get_solution_part2],
-        [day03::get_solution_part1, get_not_implemented_solution],
+        [day03::get_solution_part1, day03::get_solution_part2],
     ];
 
     return known_solutions;
