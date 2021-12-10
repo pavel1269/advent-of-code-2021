63b36b89eff1d0d0b45b6e54dd64fc2f7759df1b
pavel1269 <pavelgrim@gmail.com>
2021-12-10 09:36:22 +0100

    Day 10, part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -4,52 +4,76 @@ pub fn get_solution_part1() -> i64 {
     return result;
 }
 
-fn identify_corrupted_lines(input: &str) -> i64 {
-    #[derive(PartialEq)]
-    enum BracketType {
-        Round,  // ()
-        Square, // []
-        Joined, // {}
-        Pointy, // <>
-    }
+pub fn get_solution_part2() -> i64 {
+    let input = get_input();
+    let result = fix_incomplete(input);
+    return result;
+}
 
-    let mut squares: Vec<BracketType> = Vec::new();
-    let mut sum = 0;
+fn fix_incomplete(input: &str) -> i64 {
+    let mut scores = Vec::new();
     for input_line in input.trim().lines() {
+        let mut squares = Vec::new();
+        let mut corrupted = false;
         for char in input_line.chars() {
             match char {
-                '(' => squares.push(BracketType::Round),
-                '[' => squares.push(BracketType::Square),
-                '{' => squares.push(BracketType::Joined),
-                '<' => squares.push(BracketType::Pointy),
+                '(' => squares.push(')'),
+                '[' => squares.push(']'),
+                '{' => squares.push('}'),
+                '<' => squares.push('>'),
                 ')' | ']' | '}' | '>' => {
                     let expected = squares.pop().unwrap();
-                    match char {
-                        ')' => {
-                            if expected != BracketType::Round {
-                                sum += 3;
-                                break;
-                            }
-                        },
-                        ']' => {
-                            if expected != BracketType::Square {
-                                sum += 57;
-                                break;
-                            }
-                        },
-                        '}' => {
-                            if expected != BracketType::Joined {
-                                sum += 1197;
-                                break;
-                            }
-                        },
-                        '>' => {
-                            if expected != BracketType::Pointy {
-                                sum += 25137;
-                                break;
-                            }
-                        },
-                        _ => panic!("Unexpected char '{}'", char),
+                    if expected != char {
+                        corrupted = true;
+                        break;
+                    }
+                },
+                _ => panic!("Unexpected char '{}'", char),
+            }
+        }
+
+        if !corrupted {
+            let mut score = 0;
+            while let Some(char) = squares.pop() {
+                score *= 5;
+                score += match char {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => panic!("Unexpected char '{}'", char),
+                };
+            }
+            scores.push(score);
+        }
+    }
+
+    scores.sort();
+    
+    return scores[scores.len() / 2];
+}
+
+fn identify_corrupted_lines(input: &str) -> i64 {
+    let mut sum = 0;
+    for input_line in input.trim().lines() {
+        let mut squares = Vec::new();
+        for char in input_line.chars() {
+            match char {
+                '(' => squares.push(')'),
+                '[' => squares.push(']'),
+                '{' => squares.push('}'),
+                '<' => squares.push('>'),
+                ')' | ']' | '}' | '>' => {
+                    let expected = squares.pop().unwrap();
+                    if expected != char {
+                        sum += match char {
+                            ')' => 3,
+                            ']' => 57,
+                            '}' => 1197,
+                            '>' => 25137,
+                            _ => panic!("Unexpected char '{}'", char),
+                        };
+                        break;
                     }
                 },
                 _ => panic!("Unexpected char '{}'", char),
@@ -93,5 +117,19 @@ mod tests {
         let result = get_solution_part1();
 
         assert_eq!(442131, result);
+    }
+
+    #[test]
+    fn example_part2_correct_result() {
+        let result = fix_incomplete(get_example_input());
+
+        assert_eq!(288957, result);
+    }
+
+    #[test]
+    fn input_part2_correct_result() {
+        let result = get_solution_part2();
+
+        assert_eq!(3646451424, result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn get_known_solutions() -> Vec<[fn() -> i64; MAX_PARTS]> {
         [day07::get_solution_part1, day07::get_solution_part2],
         [day08::get_solution_part1, day08::get_solution_part2],
         [day09::get_solution_part1, day09::get_solution_part2],
-        [day10::get_solution_part1, get_not_implemented_solution],
+        [day10::get_solution_part1, day10::get_solution_part2],
     ];
 
     return known_solutions;
